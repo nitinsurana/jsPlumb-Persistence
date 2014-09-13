11bbae4a45aca57fb62ca370f97d7ccb137fc3e8
Priyanka <priyanka@kalindiinfotech.com>
2014-09-13 15:47:36 +0530

    anchor setting

--- save.js
+++ save.js
@@ -1,4 +1,5 @@
-var save = function(inst)
+
+ var save = function(inst)
     {
         var connection;
         connection = inst.getAllConnections();
@@ -7,48 +8,45 @@ var save = function(inst)
             var $elem = $(elem);
             var id = $elem.attr('id');
 //            alert(id);
-            jsPlumb.selectEndpoints({source: "#"+id}).each(function(endpoint) {
+            var end = inst.selectEndpoints({source: id}).each(function(endpoint) {
                 console.log(endpoint);
             });
-
+            console.log(end.getParameters());
             blocks.push({
                 id: $elem.attr('id'),
                 left: parseInt($elem.css("left"), 10),
                 top: parseInt($elem.css("top"), 10),
                 width: parseInt($elem.css("width"), 10),
                 heigth: parseInt($elem.css("heigth"), 10),
-                html: $elem.html(),
-                sourceEndpoints: function() {
-                    var temp = [];
-                    inst.selectEndpoints({
-                        source: id
-                    }).each(function(endpoint) {
-                        temp.push({
-                            uuid: endpoint.getUuid(),
-                            x: endpoint.anchor.x,
-                            y: endpoint.anchor.y,
-                            orientation: endpoint.anchor.orientation,
-                            offset: endpoint.anchor.offsets,
-                            parameters: endpoint.getParameters()
-                        });
-                    });
-                    return temp;
-                }()
+                html: $elem.html()
+
             });
         });
-        var connections=[];
+		 var connections=[];
         for (var i = 0; i < connection.length; i++) {
+            console.log(connection[i].endpoints[0]);
             connections.push({
                 connectionId: connection[i].id,
                 pageSourceId: connection[i].sourceId,
                 pageTargetId: connection[i].targetId,
                 sourceEndpointUuid: connection[i].endpoints[0].getUuid(),
                 targetEndpointUuid: connection[i].endpoints[1].getUuid(),
+
+                anchors: $.map(connection[i].endpoints, function(endpoint) {
+                    return [[endpoint.anchor.x,
+                            endpoint.anchor.y
+                        ]];
+                }),
                 labelText: connection[i].getLabel()
+               
             });
         }
-        var obj={connections:connections,blocks:blocks};
+        console.log(JSON.stringify(connections));
+        console.log(JSON.stringify(blocks));
+		
+		var obj={connections:connections,blocks:blocks};
         $("#textarea").val(JSON.stringify(obj));
         console.log(JSON.stringify(connections));
         console.log(JSON.stringify(blocks));
     };
+
